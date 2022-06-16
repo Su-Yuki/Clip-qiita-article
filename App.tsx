7a8e8ff4cc6cff4187eea76a4f6ed7d6c6354ec7
Su-Yuki <76637981+Su-Yuki@users.noreply.github.com>
2022-06-16 22:50:30 +0900

    Add: get contents

--- App.tsx
+++ App.tsx
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+
+import axios from 'axios';
+import ListItem from './src/components/ListItem'
+
+const limit = 20;
+const page  = 1;
+const url   = `https://qiita.com/api/v2/items?page=${page}&per_page=${limit}`;
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => { 
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios
+        .get(url, {
+          headers: {
+            "content-type": "application/json",
+            "charset":      "utf-8" 
+          }
+        });
+      setArticles(response.data);
+      // console.log(articles);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        data={articles}
+        renderItem={({item}) => (
+          <ListItem
+            title={item.title}
+            id={item.user.id}
+            profile_image_url={item.user.profile_image_url}
+            likes_count={item.likes_count}
+            created_at={item.created_at}
+          />
+        )}
+        // keyExtractor={}
+      />
+    </SafeAreaView>
+
   );
 }
 
@@ -14,7 +55,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
